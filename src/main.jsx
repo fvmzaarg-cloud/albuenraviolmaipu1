b09f146a855af3e388dc3aa5bc4fe4716c4ccdb8
fvmzaarg-cloud <fvmzaarg@gmail.com>
2026-03-19 12:27:20 -0300

    CON ESTO BASTA

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-window.GEMINI_API_KEY = "AIzaSyByRfYN7dVvBHGZgikBZcrmOY6lDgLgO6Y";
+window.GEMINI_API_KEY = "AIzaSyDcF1yT4vwfvGtyPpPwQOE-MIZH5IidZC8";
 window.GOOGLE_MAPS_API_KEY = "AIzaSyByRfYN7dVvBHGZgikBZcrmOY6lDgLgO6Y";
 import React from 'react'
 import ReactDOM from 'react-dom/client'
